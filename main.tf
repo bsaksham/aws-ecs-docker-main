a3cba710bcf7423f8fab8a7e29a05350b629b6b6
bsaksham <bhatiasaksham20@gmail.com>
2024-02-08 17:09:17 +0530

    adding updated main.tf and .tfstate files

--- main.tf
+++ main.tf
@@ -2,18 +2,106 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "express-ecs-cluster"
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "ecs_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "ecs_igw" {
+  vpc_id = aws_vpc.ecs_vpc.id
 }
 
 resource "aws_subnet" "ecs_subnet" {
-  count = 1
+  count = 2
 
-  vpc_id                  = aws_vpc.ecs_vpc.id
-  cidr_block              = "10.0.1.0/24"  
-  availability_zone       = "us-east-1a"    
+  vpc_id            = aws_vpc.ecs_vpc.id
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "ecs_route_table" {
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ecs_igw.id
+  }
+}
+
+resource "aws_route_table_association" "ecs_route_table_association" {
+  count          = 2
+  subnet_id      = aws_subnet.ecs_subnet[count.index].id
+  route_table_id = aws_route_table.ecs_route_table.id
+}
+
+resource "aws_security_group" "ecs_security_group" {
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  ingress {
+    from_port   = 4200
+    to_port     = 4200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "ecs_lb" {
+  name               = "ecs-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.ecs_subnet[*].id
+
+  tags = {
+    Name = "ecs-lb"
+  }
+}
+
+resource "aws_lb_target_group" "ecs_target_group" {
+  name     = "ecs-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.ecs_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    interval            = 10
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "ecs_listener" {
+  load_balancer_arn = aws_lb.ecs_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+  }
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "express-ecs-cluster"
 }
 
 resource "aws_ecs_task_definition" "ecs_task_definition" {
@@ -29,7 +117,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   container_definitions = jsonencode([
     {
       name  = "express-container"
-      image = "express-app:latest"
+      image = "public.ecr.aws/v1d2p3a3/express-app:latest"
       portMappings = [
         {
           containerPort = 8080
@@ -46,8 +134,12 @@ resource "aws_ecs_service" "ecs_service" {
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
   launch_type     = "FARGATE"
 
+  desired_count = 1
+
   network_configuration {
-    subnets = [aws_subnet.ecs_subnet[0].id]
+    subnets = aws_subnet.ecs_subnet[*].id
+    assign_public_ip = true
+    security_groups = [aws_security_group.ecs_security_group.id]
   }
 
   depends_on = [aws_ecs_task_definition.ecs_task_definition]
@@ -57,19 +149,15 @@ resource "aws_iam_role" "ecs_execution_role" {
   name = "express-ecs-execution-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
         Principal = {
-          Service = "ecs-tasks.amazonaws.com",
-        },
-      },
-    ],
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
   })
-}
-
-resource "aws_vpc" "ecs_vpc" {
-  cidr_block = "10.0.0.0/16"  
 }
